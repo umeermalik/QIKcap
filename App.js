0306f64aec6c40996f419a8bbf93d6f8e9253024
umeermalik <umeraziz803@gmail.com>
2024-04-28 01:52:16 +0500

    test commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <View>
+      <Text>fwgf</Text>
+    </View>
+  );
+};
+
+export default App;
